961a198f246ee6b81441e65ccc05299c0c6afa0f
Jose Hinojo <burningremedy@gmail.com>
2020-11-10 23:22:58 -0600

    Add chips filter to gdg list

--- app/src/main/java/com/example/android/gdgfinder/search/GdgListFragment.kt
+++ app/src/main/java/com/example/android/gdgfinder/search/GdgListFragment.kt
@@ -11,8 +11,10 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.android.gdgfinder.R
 import com.example.android.gdgfinder.databinding.FragmentGdgListBinding
 import com.google.android.gms.location.*
+import com.google.android.material.chip.Chip
 import com.google.android.material.snackbar.Snackbar
 
 private const val LOCATION_PERMISSION_REQUEST = 1
@@ -55,6 +57,28 @@ class GdgListFragment : Fragment() {
                     ).show()
                 }
             }
+        })
+
+        viewModel.regionList.observe(viewLifecycleOwner, Observer {
+            val chipGroup = binding.regionsList
+            val inflater = LayoutInflater.from(chipGroup.context)
+
+            val children = it.map {regionName ->
+                val chip = inflater.inflate(R.layout.region,chipGroup,false) as Chip
+                chip.text = regionName
+                chip.tag = regionName
+                chip.setOnCheckedChangeListener { button, isChecked ->
+                    viewModel.onFilterChanged(button.tag as String, isChecked)
+                }
+                chip
+            }
+            //remove current views
+            chipGroup.removeAllViews()
+            //update chips
+            for(chip in children){
+                chipGroup.addView(chip)
+            }
+
         })
 
         setHasOptionsMenu(true)
